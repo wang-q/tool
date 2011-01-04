88c6fb035b734ad105e316041524feb4977a2bb1
wangq <wangqiang1997@gmail.com>
2011-01-04 22:57:45 +0800

    new file:   replace.pl 	new file:   tool.komodoproject 	new file:   upload_backups.pl

---
+++ replace.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Pod::Usage;
+use File::Find::Rule;
+
+# axt
+my $dir = '.';
+my $find;
+my $replace;
+
+my $man  = 0;
+my $help = 0;
+
+GetOptions(
+    'help|?'    => \$help,
+    'man'       => \$man,
+    'dir=s'     => \$dir,
+    'find=s'    => \$find,
+    'replace=s' => \$replace,
+) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+
+# find out all ascii file
+my @files = File::Find::Rule->file()->ascii->in($dir);
+
+$find    = quotemeta $find;
+$replace = quotemeta $replace;
+
+for my $file (@files) {
+
+    # read in
+    open my $in_fh, "<", $file;
+    my $content = do { local $/; <$in_fh> };
+    close $in_fh;
+
+    # replace
+    my $times = $content =~ s/$find/$replace/g;
+    next unless $times;
+    print "Replace $times times in $file\n";
+
+    # write out
+    open my $out_fh, ">", $file;
+    print {$out_fh} $content;
+    close $out_fh;
+    print "\n";
+}
+
+exit;
+
+__END__
+
+=head1 NAME
+
+    replace.pl - replace strings in a directory
+
+=head1 SYNOPSIS
+
+    replace.pl [options]
+        Options:
+            --help              brief help message
+            --man               full documentation
+            --dir               directory
+            --find              find what
+            --replace           replace with
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will read the given input file(s) and do someting
+useful with the contents thereof.
+
+=cut
