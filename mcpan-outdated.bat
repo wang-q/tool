cec06f9a523c9b90fd8bf90c96c3302e1bf6e74a
wangq <wangqiang1997@gmail.com>
2011-01-06 23:33:23 +0800

    new file:   mcpan-outdated.bat

---
+++ mcpan-outdated.bat
@@ -0,0 +1,9 @@
+@rem
+@echo off
+if "%OS%" == "Windows_NT" goto WinNT
+cpan-outdated --mirror file:///e:/minicpan %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto end
+:WinNT
+cpan-outdated --mirror file:///e:/minicpan %*
+
+:end
