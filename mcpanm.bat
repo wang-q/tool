f72c47a1105fb98e43751baade01eb9862e9dd24
wang-q <wang-q@outlook.com>
2015-05-03 08:51:44 +0800

    deleted:    mcpanm.bat

--- mcpanm.bat
+++
@@ -1,9 +0,0 @@
-@rem
-@echo off
-if "%OS%" == "Windows_NT" goto WinNT
-cpanm --mirror file:///d:/minicpan --mirror-only %1 %2 %3 %4 %5 %6 %7 %8 %9
-goto end
-:WinNT
-cpanm --mirror file:///d:/minicpan --mirror-only %*
-
-:end
